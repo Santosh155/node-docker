fa2245489f2fd6982081b73880462c3d72821c60
santosh <khadkasantosh009@gmail.com>
2021-05-04 23:39:18 +0545

    database is now connected

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose
+    .connect('mongodb://santosh:mypass@mongodb:27017/?authSource=admin', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log('DB connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.get('/', (req, res) => {
     res.send('<h3>Is docker working fine? yes or no</h3>');
