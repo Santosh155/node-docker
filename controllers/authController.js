c91d3210828ac6527d8ba2837cd60e97bd52aca8
santosh <khadkasantosh009@gmail.com>
2021-05-09 01:56:00 +0545

    login route created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,36 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
 
 exports.signUp = async (req, res, next) => {
     try {
-        const newUser = await User.create(req.body);
+        const { username, password } = req.body;
+        const hashpassword = await bcrypt.hash(password, 12);
+
+        const newUser = await User.create({
+            username,
+            password: hashpassword,
+        });
         res.status(201).send({ newUser });
+    } catch (e) {
+        res.status(500).send({ message: e.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({
+                message: 'user not found',
+            });
+        }
+        const isCorrect = await bcrypt.compare(password, user.password);
+        if (!isCorrect)
+            return res.status(404).json({
+                message: 'password doesnt match',
+            });
+        res.status(200).send({ message: 'success' });
     } catch (e) {
         res.status(500).send({ message: e.message });
     }
